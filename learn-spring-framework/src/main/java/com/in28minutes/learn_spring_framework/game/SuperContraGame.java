dab706f57514016bde59566cf08209447e8a4cb8
Chinmayee Gahan <chinmayeegahan3@gmail.com>
2026-05-13 23:58:15 +0530

    feat: initial commit - GameRunner and Spring Boot setup from Ranga course

---
+++ learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/game/SuperContraGame.java
@@ -0,0 +1,21 @@
+package com.in28minutes.learn_spring_framework.game;
+
+public class SuperContraGame implements GamingConsole {
+	
+	public void up() {
+		System.out.println("Jump");
+	}
+
+	public void down() {
+		System.out.println("Go into a hole");
+	}
+	
+	public void left() {
+		System.out.println("sit down");
+	}
+
+	public void right() {
+		System.out.println("shoot a bullet");
+	}
+
+}
